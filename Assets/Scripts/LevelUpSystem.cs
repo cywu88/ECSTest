0128847fe6f4ae8966dd46bf31ccddeebbbfe854
easygamesniper <cywu88@126.com>
2021-08-30 12:25:20 +0800

    first commit

---
+++ Assets/Scripts/LevelUpSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class LevelUpSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref LevelComponent levelComponent) => {
+            levelComponent.level += 1f * Time.DeltaTime;
+
+            Debug.Log("levelComponent.level" + levelComponent.level); 
+        });
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var entity = entityManager.CreateEntity(typeof(LevelComponent));
+        entityManager.SetComponentData(entity, new LevelComponent { level = 10 });
+
+
+
+
+    }
+ 
+}
