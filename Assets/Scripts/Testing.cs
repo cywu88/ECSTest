d05c43fa22d66a07f24b6f1865414a8eaa397dfc
easygamesniper <cywu88@126.com>
2021-08-31 20:12:56 +0800

    test job

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Jobs;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Collections;
@@ -8,19 +9,47 @@ using Unity.Rendering;
 using Unity.Mathematics;
 using Unity.Jobs;
 using Unity.Collections;
+using Unity.Burst;
 
 public class Testing : MonoBehaviour
 {
     [SerializeField] private Mesh mesh;
     [SerializeField] private Material material;
 
+
+    [SerializeField] private Transform pfZombie;
+    private List<Zombie> zombieList;
+
+    public class Zombie
+    {
+        public Transform transform; 
+        public float moveY;
+    }
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+
+        zombieList = new List<Zombie>();
+        for (int i = 0; i < 1000; i++)
+        {
+            Transform zombieTransform = Instantiate(pfZombie, new Vector3(UnityEngine.Random.Range(-8f, 8f), UnityEngine.Random.Range(-5f, 5f)), Quaternion.identity);
+            zombieList.Add(new Zombie
+            {
+                transform = zombieTransform,
+                moveY = UnityEngine.Random.Range(1f, 2f)
+            }); 
+        }
+
         return;
 
-        var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
 
+
+
+        var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
 
 
         //var entity = entityManager.CreateEntity(typeof(LevelComponent));
@@ -78,6 +107,89 @@ public class Testing : MonoBehaviour
 
     [SerializeField] private bool useJobs;
 
+    private void jobtest2()
+    {
+        float startTime = Time.realtimeSinceStartup;
+
+        NativeArray<float3> positionArray = new NativeArray<float3>(zombieList.Count, Allocator.TempJob);
+        NativeArray<float> moveYarry = new NativeArray<float>(zombieList.Count, Allocator.TempJob);
+
+        for (int i = 0; i < zombieList.Count; i++)
+        {
+            positionArray[i] = zombieList[i].transform.position;
+            moveYarry[i] = zombieList[i].moveY;
+
+        }
+ 
+        ReallyToughParalle1Job reallyToughParalle1Job = new ReallyToughParalle1Job
+        {
+            deltaTime = Time.deltaTime,
+            positionArray = positionArray,
+            moveYArray = moveYarry,
+        };
+
+        JobHandle jobHandle = reallyToughParalle1Job.Schedule(zombieList.Count, 100);
+        jobHandle.Complete();
+         
+        for (int i = 0; i < zombieList.Count; i++)
+        {
+            zombieList[i].transform.position = positionArray[i];
+            zombieList[i].moveY = moveYarry[i];
+        }
+
+        positionArray.Dispose();
+        moveYarry.Dispose();
+    }
+
+    private void jobtest3()
+    {
+        float startTime = Time.realtimeSinceStartup;
+
+        //NativeArray<float3> positionArray = new NativeArray<float3>(zombieList.Count, Allocator.TempJob);
+        NativeArray<float> moveYArray = new NativeArray<float>(zombieList.Count, Allocator.TempJob);
+        TransformAccessArray transformAccessArray = new TransformAccessArray(zombieList.Count);
+
+
+        for (int i = 0; i < zombieList.Count; i++)
+        {
+            //positionArray[i] = zombieList[i].transform.position;
+            moveYArray[i] = zombieList[i].moveY;
+            transformAccessArray.Add(zombieList[i].transform);
+        }
+
+        /*
+        ReallyToughParalle1Job reallyToughParalle1Job = new ReallyToughParalle1Job
+        {
+            deltaTime = Time.deltaTime,
+            positionArray = positionArray,
+            moveYArray = moveYarry,
+        };
+
+        JobHandle jobHandle = reallyToughParalle1Job.Schedule(zombieList.Count, 100);
+        jobHandle.Complete();
+        */
+
+
+        ReallyToughParalle1JobTransforms reallyToughParalle1JobTransforms = new ReallyToughParalle1JobTransforms
+        {
+            deltaTime = Time.deltaTime,
+            moveYArray = moveYArray,
+        };
+        JobHandle jobHandle = reallyToughParalle1JobTransforms.Schedule(transformAccessArray);
+
+        for (int i = 0; i < zombieList.Count; i++)
+        {
+            //zombieList[i].transform.position = positionArray[i];
+            zombieList[i].moveY = moveYArray[i];
+
+        }
+
+        //positionArray.Dispose();
+        moveYArray.Dispose();
+        transformAccessArray.Dispose();
+    }
+
+
     private void Update()
     {
         float startTime = Time.realtimeSinceStartup;
@@ -93,19 +205,14 @@ public class Testing : MonoBehaviour
             }
             JobHandle.CompleteAll(jobHandleList);
 
-            jobHandleList.Dispose();
-
-
-
+            jobHandleList.Dispose(); 
         }
         else
         {
             for (int i = 0; i < 10; i++)
             {
                 ReallyToughTask();
-            }
-
-          
+            } 
         }
         
 
@@ -137,6 +244,67 @@ public class Testing : MonoBehaviour
         {
             float value = 0f;
             for (int i = 0; i < 50000; i++)
+            {
+                value = math.exp10(math.sqrt(value));
+            }
+        }
+    }
+
+    [BurstCompile]
+    public struct ReallyToughParalle1Job : IJobParallelFor
+    {
+        public NativeArray<float3> positionArray;
+
+        public NativeArray<float> moveYArray;
+
+        [ReadOnly] public float deltaTime;
+         
+        public void Execute(int index)
+        {
+            positionArray[index] += new float3(0, moveYArray[index] * Time.deltaTime, 0f);
+
+            if(positionArray[index].y > 5f)
+            {
+                moveYArray[index] = -math.abs(moveYArray[index]);
+            }
+
+            if(positionArray[index].y < -5f)
+            {
+                moveYArray[index] = +math.abs(moveYArray[index]);
+            }
+
+            float value = 0f;
+            for (int i = 0; i < 1000; i++)
+            {
+                value = math.exp10(math.sqrt(value));
+            }
+        } 
+    }
+
+    public struct ReallyToughParalle1JobTransforms : IJobParallelForTransform
+    { 
+        public NativeArray<float3> positionArray;
+
+        public NativeArray<float> moveYArray;
+
+        [ReadOnly] public float deltaTime;
+
+        public void Execute(int index, TransformAccess transform)
+        {
+            transform.position += new Vector3(0, moveYArray[index] * Time.deltaTime, 0f);
+
+            if (transform.position.y > 5f)
+            {
+                moveYArray[index] = -math.abs(moveYArray[index]);
+            }
+
+            if (transform.position.y < -5f)
+            {
+                moveYArray[index] = +math.abs(moveYArray[index]);
+            }
+
+            float value = 0f;
+            for (int i = 0; i < 1000; i++)
             {
                 value = math.exp10(math.sqrt(value));
             }
